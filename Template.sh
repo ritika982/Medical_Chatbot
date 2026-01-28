3714463ea42538e9b07ca1e5543dd8b941e1847b
Ritika <ritikaaa2712@gmail.com>
2026-01-29 03:03:27 +0530

    folder structure added

---
+++ Template.sh
@@ -0,0 +1,16 @@
+#CRETAING DIRECTORY
+mkdir -p src
+mkdir -p research
+#CREATING FILES
+touch src/_init_.py
+touch src/helper.py
+touch src/prompt.py
+touch .env
+touch setup.py
+touch app.py
+touch research/trial.ipynb
+ 
+touch requirements.txt
+
+echo "Directory and files created successfully."
+
